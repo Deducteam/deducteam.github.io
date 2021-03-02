ebf6c86b9ef44dbea3b11dae914cfdb315465cf6
Amélie <amelie.ledein@ensiie.fr>
2021-03-02 21:17:01 +0100

    Update personoj doc

---
+++ template_tool.php
@@ -0,0 +1,19 @@
+// Add a name for the project
+$name = "-";
+// Add a description of your project
+$description = "-";
+// Add the url of the project
+$url = "-";
+// Which version of Dedukti is your project compatible with?
+$comp_dk = "-";
+// Which version of Lambdapi is your project compatible with?
+$comp_lp = "-";
+
+// First Developer(s)
+$first_dev = "-";
+// Current Developer(s)
+$cur_dev = "-";
+// Past Developper(s) with period
+$past_dev = "-";
+// Contact(s)
+$contact = "-";
---
+++ tools_description/personoj.php
@@ -0,0 +1,19 @@
+// Add a name for the project
+$name = "personoj";
+// Add a description of your project
+$description = "People's Verification System in Dedukti";
+// Add the url of the project
+$url = "https://github.com/Deducteam/personoj";
+// Which version of Dedukti is your project compatible with?
+$comp_dk = "-";
+// Which version of Lambdapi is your project compatible with?
+$comp_lp = "1.0";
+
+// First Developer(s)
+$first_dev = "Gabriel Hondet";
+// Current Developer(s)
+$cur_dev = "Gabriel Hondet";
+// Past Developper(s) with period
+$past_dev = "-";
+// Contact(s)
+$contact = "Gabriel Hondet";
